51cd72954f20d733ae93b848c2999367207d9324
Nana Kwame <hakjamie5@gmail.com>
2024-07-12 00:54:34 +0000

    initial commit

---
+++ 1st.py
@@ -0,0 +1,33 @@
+def addition(a, b):
+    print(a+b)
+def subtraction(a, b):
+    print(a-b)
+
+
+# addition(4,5)
+# subtraction(5,4)
+
+
+cars = ['audi', 'mercedes' , 'BMW']
+
+i = 2
+
+while i != 0:
+    print("the function is still running")
+    i = int(input("enter a value"))
+    
+    
+for car in cars:
+    
+    print(car)
+
+def takeinput():
+    a = int(input("enter a value"))
+    b = int(input("enter a value"))
+    addition(a,b)
+
+
+
+takeinput()
+
+
